a2dc50c711e97b25b94c12c68f669b8b3c48a961
don <bandy@gmail.com>
2021-04-07 20:43:16 -0700

    completed second user story, bootstrap map and search

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from 'axios';
+import SearchCity from './SearchCity'
+import Container from "react-bootstrap/Container";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      citySearchedFor: '',
+      lat: '',
+      lon: ''
+    };
+  }
+
+  handleShowSearch = () => {
+    this.setState({haveWeSearchedYet: false})
+  }
+
+  handleSearch = async(citySearchedFor) => {
+    let locationResponseData = await axios.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITYEXPLORER}&q=${citySearchedFor}&format=json`);
+    // console.log(locationResponseData.data[0].display_name);
+
+    this.setState({
+      citySearchedFor: locationResponseData.data[0].display_name,
+      lat: locationResponseData.data[0].lat, 
+      lon: locationResponseData.data[0].lon, 
+    });
+  }
+
+  render() {
+    return (
+      <>
+      <SearchCity handleSearch={this.handleSearch}/>
+      <h1>{this.state.citySearchedFor}</h1>
+      <h1>{this.state.lat}</h1>
+      <h1>{this.state.lon}</h1>
+      <Container>
+         <img src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_CITYEXPLORER}&center=${this.state.lat},${this.state.lon}&zoom=10`} alt="map of city" />
+      </Container>
+      </>
+    );
+  }
 }
 
 export default App;
